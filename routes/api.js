b4001e4f4e943c05561f59b06f0a4e070fd0f1ae
tomporvaz <tomporvaz@gmail.com>
2019-10-02 20:32:10 -0400

    Add axios to package and make a call to AlphaVantage with one stock.

--- routes/api.js
+++ routes/api.js
@@ -10,14 +10,28 @@
 
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
+const axios = require('axios');
+
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+//connect to database using mongoose
 
 module.exports = function (app) {
-
+  
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
+  .get(function (req, res){
     
+    //test axios with alpha vantage api
+    const apiURL = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${req.body.stock}&apikey=${process.env.ALPHAVANTAGE_API_KEY}`
+    axios.get(apiURL)
+    .then(res => console.log(res))
+    .catch(error => console.log('Error', error));
+
+
+    //create response for only one stock ticker
+    
+    //create resonse for two stock tickers
+    
+  });
+  
 };
