09e64e38b3fe09a7bf61df1f6b76fc75212bf1a8
tomporvaz <tomporvaz@gmail.com>
2019-10-05 20:15:01 -0400

    Finished writting database logic for findUpdateStock.

--- routes/api.js
+++ routes/api.js
@@ -19,21 +19,21 @@ mongoose.set('useFindAndModify', false);
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 //connect to database using mongoose
 mongoose.connect(CONNECTION_STRING || 'mongodb://localhost/exercise-track' )
-  //from quick start guide in mongoose docs
-  let db = mongoose.connection;
-  db.on("error", console.error.bind(console, 'connection error'));
-  db.once('open', function (){
-    console.log("DB sucess using mongoose!")
-  });
-  
-  //issue schema and model
-  const stockSchema = new Schema({
-    stock: {type: String, required: true},
-    likes: {type: Number},
-    IP: [String]
-  });
+//from quick start guide in mongoose docs
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, 'connection error'));
+db.once('open', function (){
+  console.log("DB sucess using mongoose!")
+});
 
-  const Stock = mongoose.model('Stock', stockSchema);
+//issue schema and model
+const stockSchema = new Schema({
+  stock: {type: String, required: true},
+  likes: {type: Number},
+  IP: [String]
+});
+
+const Stock = mongoose.model('Stock', stockSchema);
 
 
 module.exports = function (app) {
@@ -49,8 +49,8 @@ module.exports = function (app) {
       console.log(`Stock Price: ${response.data['Global Quote']['05. price']}`)
     })
     .catch(error => console.log('Error', error));
-
-
+    
+    
     //create response for only one stock ticker
     console.log(req.ip);
     console.log(findUpdateStock(req.query.stock, req.query.like, req.ip));
@@ -58,24 +58,48 @@ module.exports = function (app) {
     //create resonse for two stock tickers
     
   });
-
+  
   const findUpdateStock = function (usersStock, usersLike, userIP) {
     let newStock = new Stock({
       stock: usersStock,
       likes: 0,
       IP: []
-    })
+    });
+    let returnedStock = {};
     console.log("we made inside the findUpdateStock function");
-
+    
     Stock.findOne({stock: usersStock}, function (err, stockDoc){
       if(err){console.error(err)};
       console.log("callback in findOne! stockDoc is next...");
       console.log(stockDoc);
-      if(usersLike && !stockDoc.includes(userIP)){
-        stockDoc.likes++;
-        stockDoc.IP.push(userIP);
-        Stock.update({_id: stockDoc._id}, stockDoc);
-      };
+      
+      //logic splits here if stock is found or not
+      if(!stockDoc){
+        if(usersLike){
+          newStock.likes++;
+        };
+        newStock.IP.push(userIP);
+        newStock.save(function (err, savedStockDoc){
+          if(err){console.error(err)};
+          returnedStock = savedStockDoc;
+        })
+      } else {
+        if(stockDoc.IP.includes(userIP)){
+          //nothing happens
+        } else { 
+          if(usersLike) {
+            stockDoc.likes++;
+          }
+          stockDoc.IP.push(userIP);
+        }
+         Stock.update({_id: stockDoc._id}, stockDoc, function(err, updatedStockDoc){
+           if(err){console.error(err)};
+           returnedStock = updatedStockDoc;
+         })
+      }
+
+    return returnedStock;  
+    
     })
   }
   
