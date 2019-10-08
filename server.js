f0612d39d5984fa9eb58302e9a6e222367631222
tomporvaz <tomporvaz@gmail.com>
2019-10-07 20:07:52 -0400

    Added contentSecurityPolicy.

--- server.js
+++ server.js
@@ -20,7 +20,14 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(helmet());
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives:{
+      defaultSrc: ["self"],
+      styleSrc: ["self"]
+    }
+  }
+}));
 
 //Index page (static HTML)
 app.route('/')
